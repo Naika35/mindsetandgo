85a029fc07cd54220f4519a7f2f0830699d55343
Naika35 <naika.ilponse@gmail.com>
2021-08-20 23:12:04 +0200

    feat(controller): add UserController

---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/admin/user", name="admin_user_browse")
+     */
+    public function browse(UserRepository $userRepository): Response
+    {
+        $users = $userRepository->findBy([], ['createdAt' => 'DESC']);
+
+        return $this->render('admin/user/browse.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/admin/user-edit/{id}", name="admin_user_edit")
+     */
+    public function edit(Request $request, User $user, EntityManager $em)
+    {
+
+        $form = $this->createFormBuilder($user)
+            ->add('email', EmailType::class)
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'Utilisateur' => 'ROLE_USER',
+                ],
+                'expanded' => true,
+                'multiple' => false,
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->flush();
+
+            $this->addFlash('success', 'Les informations de l\'utilisateur ont bien été modifiées.');
+
+            return $this->redirectToRoute('admin_user_browse');
+        }
+    }
+
+    /**
+     * @Route("/admin/user-delete/{id}", name="admin_user_delete")
+     */
+    public function delete(request $request, User $user, EntityManager $em)
+    {
+        $em->remove($user);
+        $em->flush;
+
+        $this->addFlash('success', 'Les informations de l\'utilisateur ont bien été supprimée.');
+
+        return $this->redirectToRoute('admin_user_browse');
+    }
+}
