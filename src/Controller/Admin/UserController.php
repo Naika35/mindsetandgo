ab1a38316f5acfd2d0a5052e9ab18e1720c51159
Naika35 <naika.ilponse@gmail.com>
2021-08-21 19:13:23 +0200

    feat(controller, template, form) : add user by admin

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -3,13 +3,16 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use App\Form\AdminUserAddType;
 use App\Repository\UserRepository;
+use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -26,7 +29,39 @@ class UserController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/admin/user-add", name="admin_user_add")
+     */
+    public function add(Request $request, UserPasswordHasherInterface $passwordHasher, FileUploader $fileUploader, EntityManagerInterface $em){
+        
+        $user = new User();
 
+        $form = $this->createForm(AdminUserAddType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Encodage du password
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+
+            $avatarFile = $form->get('avatar')->getData();
+            if ($avatarFile) {
+                $avatarFileName = $fileUploader->upload($avatarFile);
+                $user->setAvatar($avatarFileName);
+            }
+
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('success', 'Vous avez bien rajouter un(e) nouvel(le) utilisateur(rice)');
+
+            return $this->redirectToRoute('admin_user_browse'); // mettre la page login
+        }
+
+        return $this->render('admin/user/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 
     /**
      * @Route("/admin/user-edit/{id}", name="admin_user_edit")
@@ -69,7 +104,7 @@ class UserController extends AbstractController
     public function delete(User $user, EntityManagerInterface $em)
     {
         $em->remove($user);
-        $em->flush;
+        $em->flush();
 
         $this->addFlash('success', 'Les informations de l\'utilisateur ont bien été supprimée.');
 
