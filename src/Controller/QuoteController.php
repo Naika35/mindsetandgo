15d61716553024d8897ebdb6f9725166c64b55a0
Naika35 <naika.ilponse@gmail.com>
2021-08-25 20:35:08 +0200

    feat(controller, template): add edit delete quote without category, delete own profile

--- src/Controller/QuoteController.php
+++ src/Controller/QuoteController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Quote;
+use App\Form\QuoteType;
 use App\Repository\QuoteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,7 +27,7 @@ class QuoteController extends AbstractController
     }
 
     /**
-     * @Route("/quote/{id}"), name="quote_read", requirements={"id"="\d+"}
+     * @Route("/quote/{id}", name="quote_read", requirements={"id"="\d+"})
      */
     public function read(Quote $quote)
     {
@@ -34,6 +37,31 @@ class QuoteController extends AbstractController
     }
 
     /**
-     * @Route("/quote/")
+     * @Route("/quote/add", name="quote_add")
      */
+    public function add(Request $request, EntityManagerInterface $em)
+    {
+        $quote = new Quote();
+
+        $form = $this->createForm(QuoteType::class, $quote);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Association de la citation au user connecté
+            $quote->setUser($this->getUser());
+
+            $em->persist($quote);
+            $em->flush();
+
+            $this->addFlash('success', 'La citation a bien été rajoutée');
+
+            return $this->redirectToRoute('quote_read', ['id' => $quote->getId()]);
+        }
+
+        return $this->render('quote/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
 }
